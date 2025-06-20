22613aed37527267f92d516031a3738532bad791
jjoluwasere <jjoluwasere@gmail.com>
2025-06-20 23:24:26 +0100

    learnt about locals in terraform so decided to add this instead of adding multiple tags to each resource section. And some minor changes too.

--- ec2/networking.tf
+++ ec2/networking.tf
@@ -1,15 +1,17 @@
+locals {
+  common_tags = {
+    Name      = "resources"
+    ManagedBy = "Terraform"
+    Project   = "DevOpsGuys"
+  }
+}
+
 # IP range for the entire VPC.
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
-
-  tags = {
-    Name = "resources"
-    ManagedBy = "Terraform"
-    Project = "DevOpsGuys"
-  }
+  tags = local.common_tags
 }
-
 
 
 # subnet is linked to VPC, and uses a /24 CIDR block, is then placed in a the london region, and assigns public IPs to instances by default.
@@ -17,12 +19,30 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "eu-west-2a"
-  map_public_ip_on_launch = true 
+  map_public_ip_on_launch = true
 
-
-  tags = {
-    Name = "resources"
-    ManagedBy = "Terraform"
-    Project = "DevOpsGuys"
-  }
 }
+
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = local.common_tags
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = local.common_tags
+}
+
+
+resource "aws_route_table_association" "name" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
--- ec2/provider.tf
+++ ec2/provider.tf
@@ -2,12 +2,12 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-terraform  {
-    required_version = "~> 1.12.2" 
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~>5.99.1"
-        }  
+terraform {
+  required_version = "~> 1.12.2"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>5.99.1"
     }
+  }
 }
